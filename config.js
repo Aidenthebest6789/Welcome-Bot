74c0d36c4a87a3147ba4a374cbe183eed5a0bd97
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-01 09:27:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     token : "",
-    mongoURI : "",
+    mongoURI : "mongodb+srv://All-in-one:6306@all-in-one.mslpx.mongodb.net/?retryWrites=true&w=majority&appName=All-in-one",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
     customStatus: "HexaBot wwelcome",
