306fed32cf9f85070161b87122dc45013a7912c2
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-08 11:40:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     token : "",
   statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
-    statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
+    statusContent: "C", // A = Show total users count, B = Show total servers count, C = Custom message
     customStatus: "Support discord.gg/invite/qAE9FaPTwu",
     streamingURL: "discord.gg/qAE9FaPTwu"
 };
