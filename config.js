fb477f9f1abbc151153d45b7678e83f3ff4270e6
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-09 16:46:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     token : "",
-  statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
+   mongoURI : "mongodb+srv://All-in-one:6306@all-in-one.mslpx.mongodb.net/?retryWrites=true&w=majority&appName=All-in-one",
+statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "C", // A = Show total users count, B = Show total servers count, C = Custom message
     customStatus: "Support discord.gg/invite/qAE9FaPTwu",
     streamingURL: "discord.gg/qAE9FaPTwu"
